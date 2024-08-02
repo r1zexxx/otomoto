7fec583add686419a63563d13cf92c5f48692d3f
R1ze <plotvaon0fps@gmail.com>
2024-08-02 11:06:00 +0300

    On branch master Your branch is up to date with 'origin/master'. Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+
+print("hello")
